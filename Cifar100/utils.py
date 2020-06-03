c86dd6d3de2e9c89e029796d4dd8ac1563d32ce1
pI88Id <s257018@studenti.polito.it>
2020-06-03 15:41:48 +0200

    plot out of Cifar100

--- Cifar100/utils.py
+++ Cifar100/utils.py
@@ -50,44 +50,45 @@ class Cifar100(CIFAR100):
         self.data = np.concatenate((self.data, images), axis=0)
         self.targets = self.targets + labels
 
-    def plot(self, new_acc_train, new_acc_test, new_loss_train, new_loss_test, all_acc, args):
-        num_epochs = len(new_acc_train[0])
-        x = np.linspace(1, num_epochs, num_epochs)
 
-        for i, (acc_train, acc_test, loss_train, loss_test) in enumerate(zip(new_acc_train, new_acc_test, new_loss_train, new_loss_test)):
+def plot(self, new_acc_train, new_acc_test, new_loss_train, new_loss_test, all_acc, args):
+    num_epochs = len(new_acc_train[0])
+    x = np.linspace(1, num_epochs, num_epochs)
 
-            title = 'Accuracy dataset # %d - BATCH_SIZE= %d LR= %f  EPOCHS= %d  STEP_SIZE= %d GAMMA= %f' \
-                    % (i + 1, args['BATCH_SIZE'], args['LR'], args['NUM_EPOCHS'], args['STEP_SIZE'], args['GAMMA'])
-            title2 = 'Loss dataset # %d - BATCH_SIZE= %d LR= %f  EPOCHS= %d  STEP_SIZE= %d GAMMA= %f' \
-                     % (i + 1, args['BATCH_SIZE'], args['LR'], args['NUM_EPOCHS'], args['STEP_SIZE'], args['GAMMA'])
+    for i, (acc_train, acc_test, loss_train, loss_test) in enumerate(zip(new_acc_train, new_acc_test, new_loss_train, new_loss_test)):
 
-            plt.plot(x, acc_train, color='mediumseagreen')
-            plt.plot(x, acc_test, color='lightseagreen')
-            plt.title(title)
-            plt.xticks(np.arange(1, num_epochs, 4))
-            plt.xlabel('Epoch')
-            plt.ylabel('Accuracy')
-            plt.legend(['Train accuracy', 'Test accuracy'], loc='best')
-            plt.show()
+        title = 'Accuracy dataset # %d - BATCH_SIZE= %d LR= %f  EPOCHS= %d  STEP_SIZE= %d GAMMA= %f' \
+                % (i + 1, args['BATCH_SIZE'], args['LR'], args['NUM_EPOCHS'], args['STEP_SIZE'], args['GAMMA'])
+        title2 = 'Loss dataset # %d - BATCH_SIZE= %d LR= %f  EPOCHS= %d  STEP_SIZE= %d GAMMA= %f' \
+                 % (i + 1, args['BATCH_SIZE'], args['LR'], args['NUM_EPOCHS'], args['STEP_SIZE'], args['GAMMA'])
 
-            plt.plot(x, loss_train, color='mediumseagreen')
-            plt.plot(x, loss_test, color='lightseagreen')
-            plt.title(title2)
-            plt.xticks(np.arange(1, num_epochs, 4))
-            plt.xlabel('Epoch')
-            plt.ylabel('Loss')
-            plt.legend(['Train loss', 'Test loss'], loc='best')
-            plt.show()
-
-        plt.plot(x, all_acc, color='lightseagreen')
-        plt.title('%s incremental learning accuracy' % (args['name']))
-        plt.xticks(np.arange(1, len(all_acc), 1))
-        plt.xlabel('Iteration')
+        plt.plot(x, acc_train, color='mediumseagreen')
+        plt.plot(x, acc_test, color='lightseagreen')
+        plt.title(title)
+        plt.xticks(np.arange(1, num_epochs, 4))
+        plt.xlabel('Epoch')
         plt.ylabel('Accuracy')
-        plt.legend(['Test accuracy'], loc='best')
+        plt.legend(['Train accuracy', 'Test accuracy'], loc='best')
         plt.show()
 
-        csv_name = '%s - BATCH_SIZE= %d LR= %f  EPOCHS= %d  STEP_SIZE= %d GAMMA= %f' % (args['name'], args['BATCH_SIZE'], args['LR'], args['NUM_EPOCHS'], args['STEP_SIZE'], args['GAMMA'])
-        pd.DataFrame(all_acc).to_csv('./Results/%s.csv' % csv_name)
+        plt.plot(x, loss_train, color='mediumseagreen')
+        plt.plot(x, loss_test, color='lightseagreen')
+        plt.title(title2)
+        plt.xticks(np.arange(1, num_epochs, 4))
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend(['Train loss', 'Test loss'], loc='best')
+        plt.show()
 
-        print('Accuracy last test', new_acc_test[-1])
+    plt.plot(x, all_acc, color='lightseagreen')
+    plt.title('%s incremental learning accuracy' % (args['name']))
+    plt.xticks(np.arange(1, len(all_acc), 1))
+    plt.xlabel('Iteration')
+    plt.ylabel('Accuracy')
+    plt.legend(['Test accuracy'], loc='best')
+    plt.show()
+
+    csv_name = '%s - BATCH_SIZE= %d LR= %f  EPOCHS= %d  STEP_SIZE= %d GAMMA= %f' % (args['name'], args['BATCH_SIZE'], args['LR'], args['NUM_EPOCHS'], args['STEP_SIZE'], args['GAMMA'])
+    pd.DataFrame(all_acc).to_csv('./Results/%s.csv' % csv_name)
+
+    print('Accuracy last test', new_acc_test[-1])
